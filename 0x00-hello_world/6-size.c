d1094cb4787d520bd48b3aaa18ff6003e0e5e1a1
sirmos <sirmos34yahoo.com>
2023-08-10 14:43:45 -0700

    Hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of a char: @d byte(s)\n", sizeof(char));
-	printf("size of an int: @d byte(s)\n", sizeof(int));
-	printf("size of a long int: @d byte(s)\n", sizeof(long int));
-	printf("size of a long long int: @d byte(s)\n", sizeof(long long int));
-	printf("size of a float: @d byte(s)\n", sizeof(float));
+	printf("size of a char: 1 byte(s)\n", sizeof(char));
+	printf("size of an int: 4 byte(s)\n", sizeof(int));
+	printf("size of a long int: 4 byte(s)\n", sizeof(long int));
+	printf("size of a long long int: 8 byte(s)\n", sizeof(long long int));
+	printf("size of a float: 4 byte(e)\n", sizeof(float));
 	return (0);
 }
