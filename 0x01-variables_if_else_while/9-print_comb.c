25fc0175b40e1d8941b55cd5a7277aa445e54ac9
sirmos <sirmos34yahoo.com>
2023-08-13 07:49:39 -0700

    Variables if else and while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - Prints all possible combinations of single-digi
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	int num;
+
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
@@ -16,5 +17,6 @@ int main(void)
 		putchar(' ');
 	}
 	putchar('\n');
+
 	return (0);
 }
