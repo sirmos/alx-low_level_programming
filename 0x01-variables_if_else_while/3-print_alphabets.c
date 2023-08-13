dba967de88907298d334a759768175e0952083b9
sirmos <sirmos34yahoo.com>
2023-08-13 04:13:27 -0700

    Variables if else and while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++) 
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	for (letter = 'A'; letter <= 'Z'; letter++) 
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 	putchar('\n');
 
