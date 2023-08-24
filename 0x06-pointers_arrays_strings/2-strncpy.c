6969f33d042af0c64c8ad4dbc77855d964d081df
sirmos <sirmos34yahoo.com>
2023-08-24 03:39:31 -0700

    More pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,15 +15,16 @@ char *_strncpy(char *dest, char *src, int n)
 	i = 0;
 
 	while (src[i] != '\0' && i < n)
-			{
-			dest[i] = src[i];
-			i++;
-			}
-			while (i < n)
-			{
-			dest[i] = '\0';
-			i++;
-			}
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
-			return (dest);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
